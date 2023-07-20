24830e249c5cb5ee971bc2b83dd6f910463e0f31
Moussa Diouf <moussadioufdiallo@mail.com>
2023-07-20 12:25:02 +0000

    20 JUILLET 2023

---
+++ api/routes/hotels.js
@@ -0,0 +1,21 @@
+import express from "express";
+import Hotel from "../models/Hotel.js"
+import createError from "http-errors";
+import { createHotel, deleteHotel, getHotels, getHotel,  updateHotel } from "../controllers/hotel.js";
+import { verifyAdmin } from "../utils/verifyToken.js";
+
+
+const router = express.Router();
+
+//CREATE 
+router.post("/",verifyAdmin ,createHotel);
+///UPDATE
+router.put("/:id",verifyAdmin ,updateHotel);
+//DELETE
+router.delete("/:id",verifyAdmin ,deleteHotel);
+//GET
+router.get("/:id", getHotel);
+//GET ALL
+router.get("/", getHotels)
+
+export default router
